d95dd7fd2e8432ba1e30efd9e91a23ea1b6dde0c
Logon Aluno <logonaluno@local.com>
2019-08-13 11:58:35 -0300

    Matriz Aula 02

---
+++ 2S/eclipse-workspace/Matriz/src/Ex04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int m [] [] = new int [10] [7];
+		int r [] [] = new int [10] [7];
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite 70 números inteiros:");
+	}
+
+}
